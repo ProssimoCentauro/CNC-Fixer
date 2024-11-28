0fa449c4980d04a0ae81517a3319e0f49bf25ed2
ProssimoCentauro <rik.todaro@gmail.conm>
2024-11-28 20:00:04 +0100

    working for linux and windows

--- srcs/cnc_fix.c
+++ srcs/cnc_fix.c
@@ -5,120 +5,141 @@
 int search_char(FILE *fd, char to_find)
 {
     char c;
-    c = fgetc(fd);
-    while (c != EOF)
+    int pos;
+
+    while ((c = fgetc(fd)) != EOF)
     {
         if (c == to_find)
-            return (ftell(fd));
-        c = fgetc(fd);
+        {
+            pos = ftell(fd) - 1;
+            return (pos);
+        }
     }
-    return (0);
+    return 0;
 }
 
 float get_num(FILE *fd)
 {
-    char    c;
-    char    *str;
-    float num;
-    size_t  i;
-
-    c = fgetc(fd);
-    str = (char *)malloc(7);
-    i = 0;
-    while (c != ' ')
+    char c;
+    size_t size = 16;
+    char *str = (char *)malloc(size);
+    if (!str)
     {
-        str[i] = c;
-        c = fgetc(fd);
-        i++;
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t i = 0;
+
+    while ((c = fgetc(fd)) != EOF && c != ' ' && c != '\n')
+    {
+        if (i >= size - 1)
+        {
+            size *= 2;
+            char *new_str = realloc(str, size);
+            if (!new_str)
+            {
+                perror("Memory reallocation failed");
+                free(str);
+                exit(EXIT_FAILURE);
+            }
+            str = new_str;
+        }
+        str[i++] = c;
     }
     str[i] = '\0';
-    num = strtof(str, NULL);
+
+    float num = strtof(str, NULL);
     free(str);
-    return (num);
+    return num;
 }
 
-int main(int ac, char **av)
+int main(int argc, char **argv)
 {
-    if (ac != 2)
+    if (argc != 2)
     {
-        printf("not enough arguments!");
-        return (0);
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return (EXIT_FAILURE);
     }
 
     char path[512];
-    FILE    *nc_file;
-    float old_num_x;
-    float old_num_y;
-    float num_x;
-    float num_y;
-    int x_direction;
-    int y_direction;
-    int pos;
 
     #ifdef _WIN32
         strcpy(path, ".\\nc_file\\");
-        strcat(path, av[1]);
-    	nc_file = fopen(path, "rb+");
+        strcat(path, argv[1]);
+        FILE *nc_file = fopen(path, "rb+");
     #else
         strcpy(path, "./nc_file/");
-        strcat(path, av[1]);
-    	nc_file = fopen(path, "r+");
+        strcat(path, argv[1]);
+        FILE *nc_file = fopen(path, "r+");
     #endif
+    
+    if (!nc_file)
+    {
+        perror("Error opening file");
+        return EXIT_FAILURE;
+    }
 
-    old_num_x = 0;
-    old_num_y = 0;
-    while (42)
-    { 
+    float old_num_x = 0, old_num_y = 0;
+    float num_x, num_y;
+    int x_direction = 1, y_direction = 1;
+    int pos;
+
+    while (1)
+    {
         pos = search_char(nc_file, 'X');
         if (!pos)
-            return (0);
+            break;
+        fseek(nc_file, pos + 1, SEEK_SET);
         num_x = get_num(nc_file);
+
         if (old_num_x == 0)
         {
             old_num_x = num_x;
-            x_direction = 1;
         }
         else if (num_x < old_num_x && x_direction == 1)
         {
             x_direction = -1;
-            old_num_x = num_x;
             num_x += 0.01;
         }
         else if (num_x > old_num_x && x_direction == -1)
         {
             x_direction = 1;
-            old_num_x = num_x;
             num_x += 0.01;
         }
         old_num_x = num_x;
-        fseek(nc_file, pos, SEEK_SET);
+
+        fseek(nc_file, pos + 1, SEEK_SET);
         fprintf(nc_file, "%.3f", num_x);
-    
+        fflush(nc_file);
+
         pos = search_char(nc_file, 'Y');
         if (!pos)
-            return (0);
+            break;
+        fseek(nc_file, pos + 1, SEEK_SET);
         num_y = get_num(nc_file);
+
         if (old_num_y == 0)
         {
             old_num_y = num_y;
-            y_direction = 1;
         }
         else if (num_y < old_num_y && y_direction == 1)
         {
             y_direction = -1;
-            old_num_y = num_y;
             num_y += 0.07;
         }
         else if (num_y > old_num_y && y_direction == -1)
         {
             y_direction = 1;
-            old_num_y = num_y;
             num_y += 0.07;
         }
         old_num_y = num_y;
-        fseek(nc_file, pos, SEEK_SET);
+
+        fseek(nc_file, pos + 1, SEEK_SET);
         fprintf(nc_file, "%.3f", num_y);
+        fflush(nc_file);
     }
+
     fclose(nc_file);
-    return (0);
+    return (EXIT_SUCCESS);
 }
